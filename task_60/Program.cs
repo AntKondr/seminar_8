d85c1b7d10fe3143ed61a1b8f02fed052b349276
Anton_Kondratev <elkondro@gmail.com>
2023-02-24 12:33:12 +0700

    task_60 feature

--- task_60/Program.cs
+++ task_60/Program.cs
@@ -1,17 +1,13 @@
 ﻿Console.Clear();
 Console.Write("seminar_8 task_60\n\n");
 
-void print_array(ref int[] array){
-    for (int i = 0; i < array.Length; i++){
-        Console.Write($"{array[i]} ");
-    }
-    Console.Write("\n\n");
-}
-
-void print2Darray(ref int[,] array){
-    for (int row = 0; row < array.GetLength(0); row++){
-        for (int col = 0; col < array.GetLength(1); col++){
-            Console.Write($"{array[row, col]}\t");
+void print3Darray(ref int[,,] array3D){
+    for (int row = 0; row < array3D.GetLength(0); row++){
+        for (int col = 0; col < array3D.GetLength(1); col++){
+            for (int depth = 0; depth < array3D.GetLength(2); depth++){
+                Console.Write($"{array3D[row, col, depth]} ({row},{col},{depth})\t");
+            }
+            Console.Write("\n");
         }
         Console.Write("\n");
     }
@@ -25,10 +21,23 @@ int abs(int num){
     return num;
 }
 
-int[,] randomUnique2Darray(int rows=2, int cols=2, int min=-2, int max=2){
+bool digits_amt(int num, ref int req_dig_amt){
+    int dig_amt = 1;
+    num = num / 10;
+    while (num != 0){
+        num = num / 10;
+        dig_amt += 1;
+    }
+    if (dig_amt == req_dig_amt){
+        return true;
+    }
+    return false;
+}
+
+int[,,] randomUnique2Darray(int rows=2, int cols=2, int depths=2, int min=-9, int max=10, int digits=1){
     int unique_amt = max - min;
-    int arr_size = rows * cols;
-    if (max > min && rows > 0 && cols >0){
+    int arr_size = rows * cols * depths;
+    if (max > min && rows > 0 && cols > 0 && depths > 0){
         if (unique_amt >= arr_size){
 
             // блок формирования проверочных массивов для позитивных и негативных значений
@@ -46,47 +55,65 @@ int[,] randomUnique2Darray(int rows=2, int cols=2, int min=-2, int max=2){
             int[] unique_neg_arr = new int[abs(min) + 1];
             // блок формирования проверочных массивов для позитивных и негативных значений
 
-            int[,] array2D = new int[rows, cols];
-            for (int row = 0; row < array2D.GetLength(0); row++){
-                for (int col = 0; col < array2D.GetLength(1); col++){
+            int[,,] array3D = new int[rows, cols, depths];
+            for (int row = 0; row < array3D.GetLength(0); row++){
+                for (int col = 0; col < array3D.GetLength(1); col++){
+                    for (int depth = 0; depth < array3D.GetLength(2); depth++){
 
-                    // блок подбора уникального значения для ячейки
-                    int unique_num;
-                    while (true){
-                        unique_num = new Random().Next(min, max);
-                        if (unique_num > -1 && positives_amt > 0){
-                            while (unique_pos_arr[unique_num] == 1){
-                                unique_num = new Random().Next(0, max);
-                            }
-                            unique_pos_arr[unique_num] = 1;
-                            positives_amt -= 1;
-                            break;
+                        // блок подбора уникального значения для ячейки
+                        int unique_num;
+                        while (true){
+                            unique_num = new Random().Next(min, max);
+                            //if (digits_amt(unique_num, ref digits)){
+                                if (unique_num > -1 && positives_amt > 0){
+                                    while (unique_pos_arr[unique_num] == 1){
+                                        unique_num = new Random().Next(0, max);
+                                        // while (!digits_amt(unique_num, ref digits)){
+                                        //     unique_num = new Random().Next(0, max);
+                                        // }
+                                    }
+                                    unique_pos_arr[unique_num] = 1;
+                                    positives_amt -= 1;
+                                    break;
+                                }
+                                else if (unique_num < 0 && negatives_amt > 0){
+                                    while (unique_neg_arr[abs(unique_num)] == 1){
+                                        unique_num = new Random().Next(min, 0);
+                                        // while (!digits_amt(unique_num, ref digits)){
+                                        //     unique_num = new Random().Next(min, 0);
+                                        // }
+                                    }
+                                    unique_neg_arr[abs(unique_num)] = 1;
+                                    negatives_amt -= 1;
+                                    break;
+                                }
+                            //}
                         }
-                        else if (unique_num < 0 && negatives_amt > 0){
-                            while (unique_neg_arr[abs(unique_num)] == 1){
-                                unique_num = new Random().Next(min, 0);
-                            }
-                            unique_neg_arr[abs(unique_num)] = 1;
-                            negatives_amt -= 1;
-                            break;
-                        }
+                        array3D[row, col, depth] = unique_num;
+                        // блок подбора уникального значения для ячейки
                     }
-                    array2D[row, col] = unique_num;
-                    // блок подбора уникального значения для ячейки
                 }
             }
-            return array2D;
+            return array3D;
         }
         else {
             Console.Write("двумерный массив с уникальными значениями заданного размера с заданным диапазоном значений не может быть сформирован");
-            return new int[,] {{}};
+            return new int[,,] {};
         }
     }
     else {
         Console.Write("в функцию переданы некорректные занчения");
-        return new int[,] {{}};
+        return new int[,,] {};
     }
 }
 
-int[,] arr = randomUnique2Darray();
-print2Darray(ref arr);
+int[,,] arr = randomUnique2Darray(rows: 70, cols: 70, depths: 70, min: 0, max: 3430000, digits: 5);
+print3Darray(ref arr);
+
+
+
+// int qwerty = 17;
+// int reqqdig = 3;
+// if (digits_amt(qwerty, ref reqqdig)){
+//     Console.Write($"{qwerty} имеет {reqqdig} знака");
+// }
