a546c3335bfab824b231bf7766f5757afda4f4a8
Anton_Kondratev <elkondro@gmail.com>
2023-02-25 18:27:33 +0700

    task_61 solution

--- task_61/Program.cs
+++ task_61/Program.cs
@@ -1,2 +1,29 @@
 ﻿Console.Clear();
 Console.Write("seminar_8 task_61\n\n");
+
+
+int factorial(int n){
+    int res = 1;
+    for (int i = 1; i <= n; i++){
+        res *= i;
+    }
+    return res;
+}
+
+
+void pask_lines(ref int lines_amt){
+    for (int line = 0; line < lines_amt; line++){
+        for (int space = 0; space <= (lines_amt - line); space++){
+            Console.Write(" ");
+        }
+        for (int num_point = 0; num_point <= line; num_point++){
+            Console.Write($" {factorial(line) / (factorial(num_point) * factorial(line - num_point))}");
+        }
+        Console.Write("\n");
+    }
+}
+
+
+Console.Write("введите количество строк треугольника Паскаля: ");
+int amt = Convert.ToInt32(Console.ReadLine());
+pask_lines(ref amt);
